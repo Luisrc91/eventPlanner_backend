14937a4f6ffd94a87d6ec0a8ff53bd5f497cbabd
Luis <luisrc91@live.com>
2023-07-15 14:11:30 -0400

    finish with basic controllers

--- controllers/place_controllers.js
+++ controllers/place_controllers.js
@@ -1,30 +1,94 @@
 const express = require("express");
 const place = express.Router();
-const  {Place_data}  = require("../models");
+const { Place_data } = require("../models");
 
 place.post("/", async (req, res) => {
-    const {
+  const { place_name, place_address, picture_place, comment, ratings } =
+    req.body;
+
+  try {
+    const event = await Place_data.create({
       place_name,
       place_address,
       picture_place,
       comment,
-      ratings
-    } = req.body;
-  // console.log(guest)
-    try {
-      const event = await Place_data.create({
-        place_name,
-        place_address,
-        picture_place,
-        comment,
-        ratings
-      });
-      // console.log(event)
-      res.status(201).json(event);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Error" });
-    }
-  });
+      ratings,
+    });
 
-module.exports = place
+    res.status(201).json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+// Get all
+place.get("/", async (req, res) => {
+  try {
+    const places = await Place_data.findAll();
+    res.json(places);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+// GET by ID
+place.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const place = await Place_data.findByPk(id);
+    if (!place) {
+      return res.status(404).json({ error: "place not found" });
+    }
+    res.json(place);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+//   Update by ID
+place.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { place_name, place_address, picture_place, comment, ratings } =
+    req.body;
+
+  try {
+    const place = await Place_data.findByPk(id);
+    if (!place) {
+      return res.status(404).json({ error: "place not found" });
+    }
+
+    place.place_name = place_name;
+    place.place_address = place_address;
+    place.picture_place = picture_place;
+    place.comment = comment;
+    place.ratings = ratings;
+    await place.save();
+
+    res.json(place);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+// Delete
+place.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const place = await Place_data.findByPk(id);
+    if (!place) {
+      return res.status(404).json({ error: "place not found" });
+    }
+
+    await place.destroy();
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+module.exports = place;
