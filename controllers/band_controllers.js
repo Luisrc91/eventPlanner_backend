c79d66249318cdd4453410f072fa9d3618a42ec8
Luis <luisrc91@live.com>
2023-07-15 14:01:56 -0400

    added more router controllers

--- controllers/band_controllers.js
+++ controllers/band_controllers.js
@@ -1,27 +1,87 @@
 const express = require("express");
 const band = express.Router();
-const  {Band_data}  = require("../models");
+const { Band_data } = require("../models");
 
 band.post("/", async (req, res) => {
-    const {
+  const { band_name, genre, event_place } = req.body;
+
+  try {
+    const event = await Band_data.create({
       band_name,
       genre,
       event_place,
-    
-    } = req.body;
-  // console.log(guest)
-    try {
-      const event = await Band_data.create({
-        band_name,
-        genre,
-        event_place,
-      });
-      // console.log(event)
-      res.status(201).json(event);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Error" });
-    }
-  });
+    });
+    res.status(201).json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+// Get all bands
+band.get("/", async (req, res) => {
+  try {
+    const bands = await Band_data.findAll();
+    res.json(bands);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
 
- module.exports = band
+// Get by Id band
+band.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const band = await Band_data.findByPk(id);
+    if (!band) {
+      return res.status(404).json({ error: "Band not Found" });
+    }
+    res.json(band);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+//   Update by ID
+band.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { band_name, genre, event_place } = req.body;
+
+  try {
+    const band = await Band_data.findByPk(id);
+    if (!band) {
+      return res.status(404).json({ error: "Band Not Found" });
+    }
+
+    band.band_name = band_name;
+    band.genre = genre;
+    band.event_place = event_place;
+    await band.save();
+
+    res.json(band);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+// Delete
+band.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const band = await Band_data.findByPk(id);
+    if (!band) {
+      return res.status(404).json({ error: "Band not Found" });
+    }
+
+    await band.destroy();
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Error" });
+  }
+});
+module.exports = band;
--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const event = express.Router();
-const  {Events_data}  = require("../models");
+const { Events_data } = require("../models");
 
 // POST /events
-
 
 event.post("/", async (req, res) => {
   const {
@@ -16,9 +15,9 @@ event.post("/", async (req, res) => {
     end_time,
     description,
     picture,
-    band_name
+    band_name,
   } = req.body;
-// console.log(guest)
+  // console.log(guest)
   try {
     const event = await Events_data.create({
       event_name,
@@ -37,6 +36,90 @@ event.post("/", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Error" });
+  }
+});
+
+event.get('/', async (req, res) => {
+  try {
+    const event = await Events_data.findAll();
+    res.json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Error' });
+  }
+});
+
+// GET by ID
+event.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const event = await Events_data.findByPk(id);
+    if (!event) {
+      return res.status(404).json({ error: 'event not found' });
+    }
+    res.json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Error' });
+  }
+});
+//   Update by ID
+event.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const {  event_name,
+    event_type,
+    guest,
+    place_name,
+    date_of_event,
+    start_time,
+    end_time,
+    description,
+    picture,
+    band_name,} = req.body;
+
+  try {
+    const event = await Events_data.findByPk(id);
+    if (!event) {
+      return res.status(404).json({ error: 'event not found' });
+    }
+
+    event.event_name = event_name;
+    event.event_type = event_type;
+    event.guest = guest;
+    event.place_name = place_name;
+
+    event.date_of_event= date_of_event;
+    event.start_time=start_time;
+    event.end_time=end_time;
+    event.description=description;
+    event.picture=picture;
+    event.band_name=band_name;
+
+   
+    await event.save();
+
+    res.json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Error' });
+  }
+});
+// Delete 
+event.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const event = await Events_data.findByPk(id);
+    if (!event) {
+      return res.status(404).json({ error: 'event not found' });
+    }
+
+    await event.destroy();
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Error' });
   }
 });
 module.exports = event;
