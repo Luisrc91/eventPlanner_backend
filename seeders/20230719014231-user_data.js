5ecf78476c911ca44c8980c9901003b9696b8482
Luis <luisrc91@live.com>
2023-07-18 22:53:28 -0400

    practicing deploying

--- seeders/20230719014231-user_data.js
+++ seeders/20230719014231-user_data.js
@@ -1,25 +1,24 @@
-'use strict';
+// "use strict";
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.bulkInsert("User_data", [
+//       {
+//         user_id: "1",
+//         user_name: "Bob",
+//         email: "test@test.com",
+//         password_digest: "password",
+//       },
+//     ]);
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
-};
+//   async down(queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//   },
+// };
