3c847895efcc3f4f3c82ae80f4ffbd5de50f2fe7
Luis <luisrc91@live.com>
2023-07-19 07:59:44 -0400

    practice on deployment

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/places', placeController);
 app.use('/authentication', authController);
 
 // LISTEN
-app.listen(process.env.POSTGRES_URL, () => {
+app.listen(process.env.PORT, () => {
     console.log(`event port: ${process.env.PORT}`);
   });
 
