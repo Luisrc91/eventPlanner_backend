62d65575d5db4f0f3f88e90488ce33e0414a6458
Luis <luisrc91@live.com>
2023-07-19 20:44:02 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const { v4 } = require('uuid');
+
 const usersController = require('./controllers/user_controllers');
 const eventController = require('./controllers/event_controllers');
 const bandController = require('./controllers/band_controllers');
@@ -34,6 +36,18 @@ app.use('/authentication', authController);
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`event port: ${process.env.PORT}`);
+  });
+
+  app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+  
+  app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
   });
 
   module.exports = app;
